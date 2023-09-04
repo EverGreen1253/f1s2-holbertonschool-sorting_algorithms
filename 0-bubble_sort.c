664ec4504150aa7cf3dcc08f291aae5af9e9f1b9
Adrian <dreamweaver3d@gmail.com>
2023-09-04 15:05:23 +0800

    sorting task 0 commit 6

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -7,37 +7,54 @@
  * @array: the array
  * @size: the size
  *
+ * for (i = 0; i < size - 1; i++)
+ * {
+ *  swapped = 0;
+ *  for (j = 0; j < size - i - 1; j++)
+ *  {
+ *   if (array[j] > array[j + 1])
+ *   {
+ *    swap(&array[j], &array[j + 1]);
+ *    swapped = 1;
+ *    print_array(array, size);
+ *   }
+ *  }
+ *
+ *  if (swapped == 0)
+ *   break;
+ * }
+ *
  * Return: nothing
  */
 void bubble_sort(int *array, size_t size)
 {
-	size_t i, j;
+	size_t i;
+	/* size_t j; */
 	int swapped = 0;
+	int continue_sorting = 1;
 
 	if (array == NULL || size  == 0)
-	{
 		exit(98);
-	}
 
-	for (i = 0; i < size - 1; i++)
+	while (continue_sorting)
 	{
 		swapped = 0;
-		for (j = 0; j < size - i - 1; j++)
+		i = 0;
+		while ((i + 1) < size)
 		{
-			if (array[j] > array[j + 1])
+			if (array[i] > array[i + 1])
 			{
-				swap(&array[j], &array[j + 1]);
+				swap(&array[i], &array[i + 1]);
 				swapped = 1;
-
 				print_array(array, size);
 			}
+			i++;
 		}
 
-		/* If no two elements were swapped by inner loop, */
-		/* then break */
-		if (swapped == 0)
-			break;
+		if (swapped == 0) /* nothing left to sort! */
+			continue_sorting = 0;
 	}
+
 }
 
 /**
