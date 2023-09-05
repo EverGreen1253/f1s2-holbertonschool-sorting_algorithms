d9e8583041191915c70c4ed7443349e79b392f16
Adrian <dreamweaver3d@gmail.com>
2023-09-05 08:30:57 +0800

    sorting task 2 commit 4

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -16,17 +16,14 @@ void selection_sort(int *array, size_t size)
 	if (array == NULL || size  == 0)
 		exit(98);
 
-	// One by one move boundary of unsorted subarray
 	for (i = 0; i < size - 1; i++)
 	{
-		// Find the minimum element in unsorted array
 		min_idx = i;
 		for (j = i + 1; j < size; j++)
 			if (array[j] < array[min_idx])
 				min_idx = j;
 
-		// Swap the found minimum element with the first element
-		if(min_idx != i)
+		if (min_idx != i)
 		{
 			swap(&array[min_idx], &array[i]);
 			print_array(array, size);
